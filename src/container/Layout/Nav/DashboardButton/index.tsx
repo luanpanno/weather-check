3de279386aa841b935ca5ea5691806588f68aad8
Luan Panno <luanpanno@gmail.com>
2021-03-27 00:41:34 -0300

    feat: dashboard button tooltip

--- src/container/Layout/Nav/DashboardButton/index.tsx
+++ src/container/Layout/Nav/DashboardButton/index.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { RiMenuFill } from 'react-icons/ri';
 import OutsideClickHandler from 'react-outside-click-handler';
 
+import Tooltip from '../../../../components/Tooltip';
+import { handleTooltipId } from '../../../../shared/utils/handleTooltipId';
 import Dropdown from '../Dropdown';
 import { Container, Button } from './style';
 
@@ -15,8 +17,14 @@ const DashboardButton: React.FC = () => {
   return (
     <OutsideClickHandler onOutsideClick={() => setOpenDropdown(false)}>
       <Container>
-        <Button type="button" onClick={handleOpenDropdown}>
+        <Button
+          type="button"
+          onClick={handleOpenDropdown}
+          data-tip="Dashboard"
+          data-for={handleTooltipId('open', 'dashboard')}
+        >
           <RiMenuFill />
+          <Tooltip id={handleTooltipId('open', 'dashboard')} />
         </Button>
         {openDropdown && <Dropdown />}
       </Container>
